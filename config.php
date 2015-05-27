de37ab42ff7f54dde87dbe55db9956c337876170
wangxiaochi <wangxiaochi@baidu.com>
2015-05-27 19:40:10 +0800

    mail' => ['debug' => true

--- config.php
+++ config.php
@@ -1,7 +1,10 @@
 <?php
 
-return array('db' => [
-    'dsn' => 'mysql:host=localhost;dbname=dreamer',
-    'username' => 'root',
-    'password' => 'root',
-]);
+return array(
+    'db' => [
+        'dsn' => 'mysql:host=localhost;dbname=dreamer',
+        'username' => 'root',
+        'password' => 'root',
+    ],
+    'mail' => ['debug' => true],
+);
