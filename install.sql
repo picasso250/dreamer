8ee6ba55fb7de85ca8407a1b9e19985213eb9a0e
wangxiaochi <wangxiaochi@baidu.com>
2015-05-29 21:57:24 +0800

    w

--- install.sql
+++ install.sql
@@ -1,3 +1,10 @@
+CREATE TABLE `append` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `t_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `comment` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `content` text,
@@ -10,17 +17,17 @@ CREATE TABLE `fav` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
   `t_id` int(10) unsigned NOT NULL,
-  `is_delete` tinyint(3) unsigned NOT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_delete` tinyint(3) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `node` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `pid` int(10) unsigned NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `user_id` int(10) unsigned NOT NULL,
   `description` varchar(255) DEFAULT NULL,
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `thread` (
@@ -28,23 +35,23 @@ CREATE TABLE `thread` (
   `title` varchar(255) NOT NULL,
   `content` text,
   `node_id` int(10) unsigned NOT NULL,
+  `root_node_id` int(10) unsigned NOT NULL,
   `action_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `user_id` int(10) unsigned NOT NULL,
   `comment_count` smallint(5) unsigned NOT NULL,
   `visit_count` int(10) unsigned NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `vote_for` int(10) unsigned NOT NULL,
   `vote_against` int(10) unsigned NOT NULL,
-  `root_node_id` int(10) unsigned NOT NULL,
   `hot` int(10) unsigned NOT NULL,
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `email` varchar(145) DEFAULT NULL,
-  `password` varchar(145) NOT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `password` varchar(145) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `vote` (
@@ -54,10 +61,3 @@ CREATE TABLE `vote` (
   `attitude` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `dreamer`.`append` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `content` TEXT NOT NULL,
-  `create_time` TIMESTAMP NOT NULL,
-  PRIMARY KEY (`id`));
-ALTER TABLE `dreamer`.`append` 
-ADD COLUMN `t_id` INT UNSIGNED NOT NULL AFTER `create_time`;
