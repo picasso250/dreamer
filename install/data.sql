6abc8fca495991f89a2f8cced8136deed5bbeb41
wangxiaochi <wangxiaochi@baidu.com>
2015-05-29 22:08:02 +0800

    fix

---
+++ install/data.sql
@@ -0,0 +1,22 @@
+/*
+-- Query: SELECT * FROM dreamer.node
+LIMIT 0, 1000
+
+-- Date: 2015-05-29 22:04
+*/
+INSERT INTO `node` (`id`,`name`,`pid`,`create_time`,`user_id`,`description`) VALUES (1,'技术',0,'2015-05-27 21:34:02',0,NULL);
+INSERT INTO `node` (`id`,`name`,`pid`,`create_time`,`user_id`,`description`) VALUES (2,'创意',0,'2015-05-28 13:38:39',1,NULL);
+INSERT INTO `node` (`id`,`name`,`pid`,`create_time`,`user_id`,`description`) VALUES (3,'程序员',1,'2015-05-28 13:55:37',1,'while code monkey still code');
+INSERT INTO `node` (`id`,`name`,`pid`,`create_time`,`user_id`,`description`) VALUES (4,'好玩',0,'2015-05-28 14:30:43',1,NULL);
+INSERT INTO `node` (`id`,`name`,`pid`,`create_time`,`user_id`,`description`) VALUES (5,'分享创造',2,'2015-05-28 19:58:35',1,NULL);
+INSERT INTO `node` (`id`,`name`,`pid`,`create_time`,`user_id`,`description`) VALUES (6,'舞台',0,'2015-05-29 19:42:16',6,NULL);
+INSERT INTO `node` (`id`,`name`,`pid`,`create_time`,`user_id`,`description`) VALUES (7,'话剧',6,'2015-05-29 19:44:34',6,NULL);
+INSERT INTO `node` (`id`,`name`,`pid`,`create_time`,`user_id`,`description`) VALUES (8,'PHP',1,'2015-05-29 19:47:54',6,NULL);
+INSERT INTO `node` (`id`,`name`,`pid`,`create_time`,`user_id`,`description`) VALUES (9,'奇思妙想',2,'2015-05-29 19:45:57',6,NULL);
+INSERT INTO `node` (`id`,`name`,`pid`,`create_time`,`user_id`,`description`) VALUES (10,'分享发现',4,'2015-05-29 19:47:18',0,NULL);
+INSERT INTO `node` (`id`,`name`,`pid`,`create_time`,`user_id`,`description`) VALUES (11,'电影',4,'2015-05-29 19:47:18',0,NULL);
+INSERT INTO `node` (`id`,`name`,`pid`,`create_time`,`user_id`,`description`) VALUES (12,'剧集',4,'2015-05-29 19:47:18',0,NULL);
+INSERT INTO `node` (`id`,`name`,`pid`,`create_time`,`user_id`,`description`) VALUES (13,'音乐',4,'2015-05-29 19:47:18',0,NULL);
+INSERT INTO `node` (`id`,`name`,`pid`,`create_time`,`user_id`,`description`) VALUES (14,' 旅游',4,'2015-05-29 19:47:18',0,NULL);
+INSERT INTO `node` (`id`,`name`,`pid`,`create_time`,`user_id`,`description`) VALUES (15,'编剧',6,'2015-05-29 19:48:38',0,NULL);
+INSERT INTO `node` (`id`,`name`,`pid`,`create_time`,`user_id`,`description`) VALUES (16,'表演',6,'2015-05-29 19:48:38',0,NULL);
